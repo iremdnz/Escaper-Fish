5e495f5fd3b925f6e09af110fbb2375a72d80305
iremdnz <eda.deniz@ceng.deu.edu.tr>
2020-10-12 02:47:02 +0300

    constrait control

--- app/src/main/java/com/edairem/denemee/MainActivity.java
+++ app/src/main/java/com/edairem/denemee/MainActivity.java
@@ -20,15 +20,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Full screen
-        this.getWindow().getDecorView().setSystemUiVisibility(
+        /*this.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);*/
         //getSupportActionBar().hide();
-        //getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         highScoreText = findViewById(R.id.highScoreText);
         sharedPreferences = getApplication().getSharedPreferences("com.edairem.denemee", Context.MODE_PRIVATE);//minik bir veritabanı veren obje
         storedScore = sharedPreferences.getInt("storedScore", 0);
--- app/src/main/java/com/edairem/denemee/MainActivity2.java
+++ app/src/main/java/com/edairem/denemee/MainActivity2.java
@@ -15,13 +15,13 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         //Full screen
-        this.getWindow().getDecorView().setSystemUiVisibility(
+        /*this.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);*/
         //getSupportActionBar().hide();
     }
 
