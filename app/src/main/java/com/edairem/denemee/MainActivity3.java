136c526dff6fc74e8ead59a47cf816d447efbcd9
iremdnz <eda.deniz@ceng.deu.edu.tr>
2020-10-12 01:24:55 +0300

    activation and navigation bar settings

--- app/src/main/java/com/edairem/denemee/MainActivity3.java
+++ app/src/main/java/com/edairem/denemee/MainActivity3.java
@@ -59,13 +59,13 @@ public class MainActivity3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
         //Full screen
-        /*this.getWindow().getDecorView().setSystemUiVisibility(
+        this.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);*/
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         //getSupportActionBar().hide();
         Intent intent = getIntent();
         speed = intent.getIntExtra("speed",0);
